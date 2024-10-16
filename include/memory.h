9f8cc5d98c8cfc6a54e77f2fe2490c880905d23f
hanyuu <hanyuujp5192@protonmail.com>
2024-10-16 17:41:23 +0100

    corrected variables, insertion of PROGRAM_SIZE

--- include/memory.h
+++ include/memory.h
@@ -4,10 +4,11 @@
 
 // constants
 
-#define MEMORY_START 0x200ul	 	// 512
-#define ETI660_START 0x600ul	 	// 1536
-#define INTERPRETER_MEMORY 0x200ul	// 512
-#define MEMORY_SIZE (0xFFFul - MEMORY_START) // 4096 - 512
+#define MEMORY_START 0x200ul	 				  // 512
+#define ETI660_START 0x600ul	 	 			  // 1536
+#define INTERPRETER_MEMORY 0x200ul				  // 512
+#define MEMORY_SIZE 0xFFFul						  // 4096 
+#define PROGRAM_SIZE (MEMORY_SIZE - MEMORY_START) // 4096 - 512
 
 extern uint8_t SP;
 extern uint8_t PC;
